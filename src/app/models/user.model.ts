e0c8962de97acbc818bf97f6f1f5ba0948cf6461
Fede <gauna.federico.nicolas@gmail.com>
2025-01-04 14:13:49 -0300

    Add user model

---
+++ src/app/models/user.model.ts
@@ -0,0 +1,11 @@
+
+
+export class User {
+    public name: string;     
+    public email: string;     
+    public password?: string;     
+    public img?: string;     
+    public google?: string;     
+    public role?: string;     
+    public id?: string;
+} {}
